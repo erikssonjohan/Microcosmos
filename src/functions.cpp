579ac15d1d1fee26bb545559368c1288981b29a5
martin orava <martin orava>
2017-03-21 11:53:02 +0100

    fixed xml for windows

--- src/functions.cpp
+++ src/functions.cpp
@@ -3,6 +3,7 @@
 //
 
 #include "functions.h"
+#include <iostream>
 
 void functions::xml(const char *file, std::vector<CardHandler> &cards) {
     //TODO:: add checks if for example the path has spaces or other thing that we don't want
@@ -21,7 +22,8 @@ void functions::xml(const char *file, std::vector<CardHandler> &cards) {
                 path = child->Attribute("path");
             }
             if (child->Attribute("scale_exp")) {
-                scale = (float) std::stod(child->Attribute("scale_exp"));
+                float scale = std::atof(child->Attribute("scale_exp"));
+                //f(float) std::stod(child->Attribute("scale_exp"));
             }
             if (child->FirstChildElement("category")) {
                 categories.push_back(child->FirstChildElement("category")->Attribute("name"));
--- src/main.cpp
+++ src/main.cpp
@@ -33,47 +33,47 @@ int main(int argc, char* args[]) {
 
 
 
-	//SDL Window
-	SDL_Window* window = nullptr;
+    //SDL Window
+    SDL_Window* window = nullptr;
 
-	//The surface contained by the window
-	SDL_Surface* screenSurface = nullptr;
+    //The surface contained by the window
+    SDL_Surface* screenSurface = nullptr;
 
-	//Initialize SDL
-	if (SDL_Init(SDL_INIT_VIDEO) < 0)
-	{
+    //Initialize SDL
+    if (SDL_Init(SDL_INIT_VIDEO) < 0)
+    {
         SDL_GetError();
-	}
-	else
-	{
-		//Create window
-		window = SDL_CreateWindow("SDL", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, SCREEN_WIDTH, SCREEN_HEIGHT, SDL_WINDOW_SHOWN);
-		if (window == nullptr)
-		{
-			SDL_GetError();
-		}
-		else
-		{
-			//Get window surface
-			screenSurface = SDL_GetWindowSurface(window);
+    }
+    else
+    {
+        //Create window
+        window = SDL_CreateWindow("SDL", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, SCREEN_WIDTH, SCREEN_HEIGHT, SDL_WINDOW_SHOWN);
+        if (window == nullptr)
+        {
+            SDL_GetError();
+        }
+        else
+        {
+            //Get window surface
+            screenSurface = SDL_GetWindowSurface(window);
 
-			//Fill the surface white
-			SDL_FillRect(screenSurface, nullptr, SDL_MapRGB(screenSurface->format, 0xFF, 0xFF, 0xFF));
+            //Fill the surface white
+            SDL_FillRect(screenSurface, nullptr, SDL_MapRGB(screenSurface->format, 0xFF, 0xFF, 0xFF));
 
-			//Update the surface
-			SDL_UpdateWindowSurface(window);
+            //Update the surface
+            SDL_UpdateWindowSurface(window);
 
-			//Wait two seconds
-			SDL_Delay(3000);
-		}
-	}
+            //Wait two seconds
+            SDL_Delay(3000);
+        }
+    }
 
-	//Destroy window
-	SDL_DestroyWindow(window);
+    //Destroy window
+    SDL_DestroyWindow(window);
 
-	//Quit SDL subsystems
-	SDL_Quit();
+    //Quit SDL subsystems
+    SDL_Quit();
 
-	return 0;
-	
+    return 0;
+
 }
