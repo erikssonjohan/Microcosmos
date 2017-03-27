20b69ab4bc6d022d2a6bd04db5805b30ddee5355
Johan Eriksson <johan@minasidor.com>
2017-03-27 16:22:17 +0200

    Added som doxygen comments

--- include/CardHandler.h
+++ include/CardHandler.h
@@ -2,6 +2,7 @@
 // Created by Oscar Nord on 2017-03-08.
 //
 
+
 //include SDL
 #include "SDL.h"
 //include GLEW
@@ -13,15 +14,28 @@
 #include <vector>
 #include <stdio.h>
 
-
 #ifndef SDL_TEST_CARDHANDLER_H
 #define SDL_TEST_CARDHANDLER_H
 
-
 class CardHandler {
 public:
+    /*!
+     * a constructor of some sort that might not be used ? idk...
+     */
     CardHandler();
 
+    /*!
+     *
+     * A constructor for the CardHandler just based on the XML
+     *
+     * @param path - path to the media (image/film)
+     * @param scale - scaling of the media
+     * @param categories - the contents categories
+     * @param header_se - the header/name of the content (se)
+     * @param text_se - the text of the content (se)
+     * @param header_en he header/name of the content (en)
+     * @param text_en - the text of the content (en)
+     */
     CardHandler(std::string path, float scale, std::vector<std::string> categories, std::string header_se,
                 std::string text_se, std::string header_en, std::string text_en );
 
@@ -29,6 +43,9 @@ public:
 
     virtual ~CardHandler();
 
+    /*!
+     * Prints the @p path, @p scale, @p categories, @p header_se, @p text_se, @p header_en and @p text_en in the console
+     */
     void displayContent();
 
     void eventHandler();
