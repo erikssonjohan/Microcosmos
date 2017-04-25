dd4c2bf17371a104e52264bef6ee2a6bcc85fc1f
Oscar Nord <oscar.nord@gmail.com>
2017-04-25 14:02:21 +0200

    Added tracking class

--- MicroCosmos/src/MicroCosmosApp.cpp
+++ MicroCosmos/src/MicroCosmosApp.cpp
@@ -13,6 +13,7 @@
 #include "MicroCosmos.hpp"
 #include "aruco/src/cvdrawingutils.h"
 #include "aruco/src/cameraparameters.h"
+#include "Tracking.hpp"
 
 
 using namespace ci;
@@ -29,30 +30,13 @@ public:
     
     //po
     po::scene::SceneRef scene;
-    
-private:
-    //aruco
-    void printDevices();
-    aruco::CameraParameters mCamParam;
-    aruco::MarkerDetector mMarkerDetector;
-    vector<aruco::Marker> mMarkers;
-    
-    CaptureRef mCapture;
-    Surface mSurf;
-    gl::TextureRef mTexture;
+    //tracking
+    Tracking mTrack;
 };
 
 
-void MicroCosmosApp::printDevices()
-{
-    for( const auto &device : Capture::getDevices() ) {
-        console() << "Device: " << device->getName() << " " << endl;
-    }
-}
-
-
 void prepareSettings( MicroCosmosApp::Settings *settings )
-{
+{    
     // By default, multi-touch is disabled on desktop and enabled on mobile platforms.
     // You enable multi-touch from the SettingsFn that fires before the app is constructed.
     settings->setMultiTouchEnabled( true );
@@ -62,16 +46,8 @@ void MicroCosmosApp::setup()
 {
     // cinder::app::setFullScreen();
     scene = Scene::create(MicroCosmos::create());
-    
-    //print cameras and init camera capture
-    printDevices();
-    try {
-        mCapture = Capture::create( 640, 480 );
-        mCapture->start();
-    }
-    catch( ci::Exception &exc ) {
-        CI_LOG_EXCEPTION( "Failed to init capture ", exc );
-    }
+    //search for and start camerafeed
+    mTrack.setup();
 }
 
 
@@ -95,25 +71,13 @@ void MicroCosmosApp::update()
 {
     //update scene
     scene->update();
-    
-    //aruco
-    if( mCapture && mCapture->checkNewFrame()) {
-        if(!mTexture) {
-            // Capture images come back as top-down, and it's more efficient to keep them that way
-            mTexture = gl::Texture::create(*mCapture->getSurface(), gl::Texture::Format().loadTopDown());
-            mSurf = Surface(Channel8u(mTexture->createSource()));
-        }
-        else {
-            mTexture->update(*mCapture->getSurface());
-            cv::Mat input = toOcv(Surface(Channel8u(mTexture->createSource())));
-            mMarkerDetector.detect(input, mMarkers);
-        }
-    }
+    //update tracking
+    mTrack.update();
 }
 
 void MicroCosmosApp::draw()
 {
-    
+   
     gl::clear( Color( 0, 0.5, 0 ) );
     scene->draw();
     
@@ -125,34 +89,8 @@ void MicroCosmosApp::draw()
     }
     
     
-    /*
-    gl::enableAlphaBlending();
-    gl::color( ColorA(1,1,1,1.0f) );
-    
-    if( mTexture ) {
-        gl::draw( mTexture );
-    }
-    
-    gl::color( ColorA(1,1,1,0.7f) );
-    
-    double pos[3];
-    double rot[4];
-    
-    for( int i=0; i<mMarkers.size(); i++) {
-        
-        for( int ii = 0; ii < mMarkers[i].size(); ii++){
-          gl::drawSolidCircle(vec2(mMarkers[i][ii].x, mMarkers[i][ii].y), 10.0f);
-            std::cout << "Marker x: " << mMarkers[i][ii].x << " Marker y: " << mMarkers[i][ii].y << endl;
-        
-        }
-        
-        
-        //mMarkers[i].OgreGetPoseParameters(pos, rot);
-        //cout << "Pos " << "x: " << pos[1] << " y: "  << pos[2] << " z: " << pos[3] << endl;
-        //cout << mMarkers[i].id << endl;
-        //gl::drawSolidCircle(vec2(pos[1],pos[2]), 10.0f);
-    }
-    */
+    //display output from camera
+    //mTrack.draw();
 }
 
 CINDER_APP( MicroCosmosApp, RendererGl, prepareSettings )
---
+++ MicroCosmos/src/Tracking.hpp
@@ -0,0 +1,49 @@
+//
+//  Tracking.hpp
+//  MicroCosmos
+//
+//  Created by Oscar Nord on 2017-04-25.
+//
+//
+
+#ifndef Tracking_hpp
+#define Tracking_hpp
+
+#include "cinder/app/App.h"
+#include "cinder/app/RendererGl.h"
+#include "cinder/gl/gl.h"
+#include "cinder/Capture.h"
+#include "cinder/gl/Texture.h"
+#include "cinder/Log.h"
+
+#include "CinderOpenCV.h"
+#include "aruco/src/cvdrawingutils.h"
+#include "aruco/src/cameraparameters.h"
+
+using namespace ci;
+using namespace ci::app;
+using namespace std;
+
+
+class Tracking {
+public:
+    
+    //Tracking();
+    //~Tracking();
+    
+    void setup();
+    void update();
+    void draw();
+    void printDevices();
+    
+private:
+    aruco::CameraParameters mCamParam;
+    aruco::MarkerDetector mMarkerDetector;
+    vector<aruco::Marker> mMarkers;
+    
+    CaptureRef mCapture;
+    Surface mSurf;
+    gl::TextureRef mTexture;
+};
+
+#endif /* Tracking_hpp */
